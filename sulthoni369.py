439546c4592c123f77c4e25e180ce2ccdeeb0e38
Sulthoni369 <145558092+Sulthoni369@users.noreply.github.com>
2023-09-20 20:30:54 +0700

    Create sulthoni369.py

---
+++ sulthoni369.py
@@ -0,0 +1,48 @@
+AUTOMATIC1111
+stable-diffusion-webui
+sd-webui
+sd-webui-controlnet
+stable-diffusion-webui-images-browser
+sd-webui
+sd-webui-cutoff
+sd-webui-regional-prompter
+sd-webui-tunnels
+sd-webui-roop
+pycloudflared
+insightface==0.7.3
+onnx==1.14.0
+onnxruntime==1.15.0
+opencv-python==4.7.0.72
+tqdm
+https://github.com/AUTOMATIC1111/stable-diffusion-webui
+https://github.com/Iyashinouta/sd-model-downloader
+https://github.com/Coyote-A/ultimate-upscale-for-automatic1111
+https://github.com/Mikubill/sd-webui-controlnet
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/config.json
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/models/ESRGAN
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/models/ESRGAN/upscalers.zip https://huggingface.co/datasets/AI-C/Data-File/resolve/main/upscalers.zip
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/models/ESRGAN
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/models/ESRGAN/upscalers.zip
+upscalers.zip
+https://github.com/IDEA-Research/DWPose
+https://github.com/fkunn1326/openpose-editor
+https://github.com/Bing-su/adetailer
+https://github.com/hnmr293/sd-webui-cutoff
+install xformers==0.0.20 triton==2.0.0 -U
+uninstall -y torch
+uninstall -y torchvision
+uninstall -y torchaudio
+install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+apt -y update -qq
+apt -y install -qq aria2 libcairo2-dev pkg-config python3-dev
+apt-get update
+apt-get install -y google-perftools
+apt -y install wget git python3 python3-venv
+/home/studio-lab-user/sagemaker-studiolab-notebooks/
+pip install mediapipe
+pip install ultralytics
+pip install insightface==0.7.3
+--xformers --share --disable-safe-unpickle --theme=dark --no-half-vae --enable-insecure-extension --gradio-queue --lowvram
+https://github.com/etherealxx/batchlinks-webui
+https://github.com/DominikDoom/a1111-sd-webui-tagcomplete
+-y -c conda-forge opencv
